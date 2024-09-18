186060fbde0030201a5fd306cee3b582aaf823d8
João Antônio <141924557+joaooferreiraa@users.noreply.github.com>
2024-09-18 14:23:39 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Qual é o maior campeão do Brasil?',
+    'Palmeiras'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual o maior time do Brasil?',
+    'Palmeiras'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem foi o último vice do Palmeiras?',
+    'Santos'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual é o maior freguês do Palmeiras?',
+    'Corinthians'
+)
